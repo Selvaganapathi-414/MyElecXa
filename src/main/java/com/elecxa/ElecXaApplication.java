3f5b0b9905807ef99cfbf1d23bb89254b3342515
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-27 21:37:19 +0530

    Initial commit - Adding Eclipse project

---
+++ src/main/java/com/elecxa/ElecXaApplication.java
@@ -0,0 +1,13 @@
+package com.elecxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecXaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecXaApplication.class, args);
+	}
+
+}
