387a65dff71b254574a5e9233b5c3f2dd6770bae
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-28 11:44:21 +0530

    Changes in Selva.java

--- src/main/java/com/elecxa/Selva.java
+++ src/main/java/com/elecxa/Selva.java
@@ -3,5 +3,7 @@ package com.elecxa;
 public class Selva {
 	public static void main(String[] args) {
 		System.out.println("Hello Spring");
+		System.out.println("Hello Spring..........SpringBoot");
+
 	}
 }
