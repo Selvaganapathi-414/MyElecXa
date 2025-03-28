cb062bc4c69c02c3cda8349d37e05c51c1002084
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-28 12:02:56 +0530

    commited a file

--- src/main/java/com/elecxa/Selva.java
+++ src/main/java/com/elecxa/Selva.java
@@ -3,9 +3,13 @@ package com.elecxa;
 public class Selva {
 	public static void main(String[] args) {
 		System.out.println("Hello Spring");
+<<<<<<< HEAD
 
 		System.out.println("Hello Spring.....SpringBoot");
 
+=======
+		System.out.println("Hello Spring..........Spring");
+>>>>>>> 52feda0 (Commit from Selva at Selva.java)
 
 	}
 }
