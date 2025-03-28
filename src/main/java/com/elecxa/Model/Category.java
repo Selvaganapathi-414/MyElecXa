dead25db6f5196e2f57c27250604ce30ba8621c2
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-28 14:47:08 +0530

    Created Entity classes for our Application

---
+++ src/main/java/com/elecxa/Model/Category.java
@@ -0,0 +1,25 @@
+package com.elecxa.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<SubCategory> subcategories;
+}
