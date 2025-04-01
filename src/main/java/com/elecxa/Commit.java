ed4598995cae8c4c68aa45f7e32ca9a190236cad
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-04-01 12:13:22 +0530

    Changes on the Model classes

--- src/main/java/com/elecxa/Commit.java
+++
@@ -1,8 +0,0 @@
-package com.elecxa;
-
-public class Commit {
-	public static void main(String[]args) {
-		System.out.println("Git and GitHub Initiation.......");
-	}
-
-}
--- src/main/java/com/elecxa/ElecXaMVCApplication.java
+++ src/main/java/com/elecxa/ElecXaMVCApplication.java
@@ -8,7 +8,6 @@ public class ElecXaMVCApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElecXaMVCApplication.class, args);
-		System.out.println("Hello Spring");
 	}
 
 }
